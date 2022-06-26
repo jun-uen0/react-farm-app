b95190deb2b5190ea83262a0a2cd9db3caa642d5
jun-uen0 <hello@junueno.dev>
2022-06-26 18:29:07 +0900

    disabled csrf handling and added routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import { useEffect } from 'react'
 import axios from 'axios'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { CsrfToken } from './types/types'
-import { useAppSelector } from './app/hooks'
-import { selectCsrfState } from './slices/appSlice'
+import { Auth } from './components/Auth'
+import { Todo } from './components/Todo'
+// import { CsrfToken } from './types/types'
+// import { useAppSelector } from './app/hooks'
+// import { selectCsrfState } from './slices/appSlice'
 
 
 function App() {
-  const csrf = useAppSelector(selectCsrfState)
-  useEffect(() => {
-    const getCsrfToken = async () => {
-      const res = await axios.get<CsrfToken>(
-        `${process.env.REACT_APP_API_URL}/csrf-token`
-      )
-      axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token
-      console.log("res.data.csrf_token: " + res.data.csrf_token)
-    }
-    getCsrfToken()
-  }, [csrf])
+  // const csrf = useAppSelector(selectCsrfState)
+  // useEffect(() => {
+  //   const getCsrfToken = async () => {
+  //     const res = await axios.get<CsrfToken>(
+  //       `${process.env.REACT_APP_API_URL}/csrf-token`
+  //     )
+  //     axios.defaults.headers.common['X-CSRF-Token'] = res.data.csrf_token
+  //     console.log("res.data.csrf_token: " + res.data.csrf_token)
+  //   }
+  //   getCsrfToken()
+  // }, [csrf])
   return (
-    <div></div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Auth />
+          </Route>
+          <Route path="/todo">
+            <Todo />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
