28e0fce033ffc32d50994f721b150c56aea3775b
jun-uen0 <hello@junueno.dev>
2022-06-26 13:52:40 +0900

    fmt and remove default feature code

--- src/app/store.ts
+++ src/app/store.ts
@@ -5,13 +5,13 @@ export const store = configureStore({
   reducer: {
     app: appReducer,
   },
-});
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
