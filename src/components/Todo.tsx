647972d0325264d72b8f8cfaa8aaba85940c4a05
jun-uen0 <hello@junueno.dev>
2022-07-10 14:44:01 +0900

    added logout button

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import { VFC, useState } from 'react'
+import { LogoutIcon } from '@heroicons/react/outline'
+import { ShieldCheckIcon } from '@heroicons/react/solid'
+import { useAppSelector, useAppDispatch } from '../app/hooks'
+import { setEditedTask, selectTask } from '../slices/appSlice'
+import { useProcessAuth } from '../hooks/useProcessAuth'
+// import { useProcessTask } from '../hooks/useProcessTask'
+// import { useQueryTasks } from '../hooks/useQueryTasks'
+// import { useQueryUser } from '../hooks/useQueryUser'
+// import { useQuerySingleTask } from '../hooks/useQuerySingleTask'
+// import { TaskItem } from './TaskItem'
 
-export const Todo = () => {
-  return <div>Todo</div>
+export const Todo: VFC = () => {
+  const { logout } = useProcessAuth()
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <LogoutIcon
+        onClick={logout}
+        className="h-8 w-8 mr-2 text-blue-500"
+      />
+    </div>
+  )
 }
